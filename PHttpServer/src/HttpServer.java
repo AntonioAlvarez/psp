1ac049303ac9eae0b2df645c4a318dc1ccf60988
Your NametoniAlvarez <xacon_93@hotmail.com>
2013-01-25 10:10:39 +0100

    subiendo PHttpServer,ahora se le indica el nombre de archivo

--- PHttpServer/src/HttpServer.java
+++ PHttpServer/src/HttpServer.java
@@ -7,125 +7,15 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-
-
-
+/*
+//si tenemos muchas peticiones con un solo hilo, hasta
+//que no termine de resolverse la primera peticion no
+//nos ponemos a escuchar la siguiente
+ * 
+*/
 public class HttpServer {
 	
-	public static void monoHilo() throws IOException{
-		
-		final String newLine = "\r\n";
-		int port = 8080;
-		final String fileNameError404 = "fileError404.html";
-		final String response200 = "HTTP/1.0 200 OK";
-		final String response404 = "HTTP/1.0 404 Not Found";
-		ServerSocket serverSocket = new ServerSocket(port);
-		Socket socket = serverSocket.accept();
-		
-		Scanner scanner = new Scanner (socket.getInputStream());
-		String fileName="index.html";
-		
-		while (true/*scanner.hasNextLine()*/)
-		{
-			String line = scanner.nextLine();//devuelve la linea
-			//if(line.startsWith("GET"))
-			System.out.println(line);
-			if(line.equals(""))
-				break;
-		}
-		
-		File file = new File(fileName);
-		
-		String responseFileName = file.exists() ? fileName : fileNameError404;
-		String response = file.exists() ? response200 : response404;
-		
-		String header = response + newLine + newLine;
-		byte [] headerBuffer = header.getBytes();
-		
-		OutputStream outputStream = socket.getOutputStream();
-		outputStream.write(headerBuffer);
-		
-		final int bufferSize = 2048;
-		byte [] buffer= new byte [bufferSize];
-		
-		FileInputStream fileInputStream = new FileInputStream(responseFileName);
 
-		int count;
+	
 
-		while ( (count = fileInputStream.read(buffer)) != -1 )
-			outputStream.write(buffer, 0, count);
-
-		
-		PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
-		printWriter.println("HTTP/1.0 404 NOT FOUND" + /*newLine*/"\n");
-		printWriter.println(""/*newLine*/);
-		
-		printWriter.close();
-		serverSocket.close();
-		
-		socket.close();
-	}
-	
-public static void multiHilo() throws IOException{
-		
-		final String newLine = "\r\n";
-		int port = 8080;
-		final String fileNameError404 = "fileError404.html";
-		final String response200 = "HTTP/1.0 200 OK";
-		final String response404 = "HTTP/1.0 404 Not Found";
-		ServerSocket serverSocket = new ServerSocket(port);
-		
-		while(true){
-			Socket socket = serverSocket.accept();
-			
-			Scanner scanner = new Scanner (socket.getInputStream());
-			String fileName="index.html";
-			
-			while (true/*scanner.hasNextLine()*/)
-			{
-				String line = scanner.nextLine();//devuelve la linea
-				//if(line.startsWith("GET"))
-				System.out.println(line);
-				if(line.equals(""))
-					break;
-			}
-			
-			File file = new File(fileName);
-			
-			String responseFileName = file.exists() ? fileName : fileNameError404;
-			String response = file.exists() ? response200 : response404;
-			
-			String header = response + newLine + newLine;
-			byte [] headerBuffer = header.getBytes();
-			
-			OutputStream outputStream = socket.getOutputStream();
-			outputStream.write(headerBuffer);
-			
-			final int bufferSize = 2048;
-			byte [] buffer= new byte [bufferSize];
-			
-			FileInputStream fileInputStream = new FileInputStream(responseFileName);
-	
-			int count;
-	
-			while ( (count = fileInputStream.read(buffer)) != -1 )
-				outputStream.write(buffer, 0, count);
-	
-			
-			PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
-			printWriter.println("HTTP/1.0 404 NOT FOUND" + /*newLine*/"\n");
-			printWriter.println(""/*newLine*/);
-			
-			printWriter.close();
-			serverSocket.close();
-			
-			socket.close();
-		}
-	}
-	
-	public static void main(String [] args) throws IOException{
-		
-		monoHilo();
-		
-	}
 }
